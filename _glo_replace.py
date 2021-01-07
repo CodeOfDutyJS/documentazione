23d91facb292f16442d1bd0ae34e693db4066633
Damiano Zanardo <damiano.zanardo@gmail.com>
2021-01-07 10:30:19 +0100

    Analisi dei requisiti (#43)
    * Update on Overleaf.
    
    * Update on Overleaf.
    
    * Update on Overleaf.
    
    * Update on Overleaf.
    
    * update script glossario
    
    * Update on Overleaf.
    
    * Update on Overleaf.
    
    * Update on Overleaf.
    
    * Aggiornata versione
    
    * Update on Overleaf.
    
    * Corretto typo
    
    * Update on Overleaf.
    
    * corretti errori
    
    * Update on Overleaf.
    
    * Update changelog.tex
    
    * aggiornato uc7
    
    * run aspell for spell checking
    
    * Update descrizione_generale.tex
    
    * corretto typo
    
    * corretto typo
    
    * corretto frasi
    
    * Update uc7.tex
    
    Co-authored-by: andrea.masca99 <andrea.masca99@gmail.com>

--- _glo_replace.py
+++ _glo_replace.py
@@ -1,44 +1,72 @@
 import sys
 import os
+import re
 
 textPath = os.getcwd() + sys.argv[1]
-gloPath= sys.argv[2]
-fileList=[]
+gloPath = sys.argv[2]
+fileList = []
 for root, dir, files in os.walk(textPath):
     for file in files:
         if file.endswith(".tex"):
-            fileList.append(os.path.join(root,file))
+            fileList.append(os.path.join(root, file))
 
 for f in fileList:
     try:
-        text = open( f , "r+")
+        text = open(f, "r+")
     except:
         print("failed to open text file")
     try:
         glossary = open(gloPath, "r")
     except:
         print("failed to open glossary file")
-    glossaryList=glossary.read().splitlines()
+    glossaryList = glossary.read().splitlines()
     glossary.close()
-    textData=text.read()
+    textData = text.read()
 
     for word in glossaryList:
-        wordReplacement= word + "\glo{}"
-        textData = textData.replace(word + " ",wordReplacement+ " ")
-        textData = textData.replace(word+ ",",wordReplacement+",")
+        wordReplacement = word + "\glo{}"
+        textData = textData.replace(word + " ", wordReplacement + " ")
+        textData = textData.replace(word + ",", wordReplacement+",")
         textData = textData.replace(word + ".", wordReplacement+".")
         textData = textData.replace(word + ":", wordReplacement + ":")
         textData = textData.replace(word + "(", wordReplacement + "(")
         textData = textData.replace(word + ")", wordReplacement + ")")
-        #textData = textData.replace("{" + word + "}", "{" + wordReplacement + "}")
-        #textData = textData.replace("{" + word, "{" + wordReplacement)
         textData = textData.replace(word + "}", wordReplacement + "}")
         textData = textData.replace(word + "\n", wordReplacement + "\n")
         textData = textData.replace(word + ";", wordReplacement + ";")
         textData = textData.replace(word + "\"", wordReplacement + "\"")
 
+    m = re.search("(startTable(.|\n)*?endTable)", textData)
+    if m:
+        print(m.group(1))
+        if m.group(1):
+            found = m.group(1)
+            newtext = found
+            for word in glossaryList:
+                wordReplacement = word + "\\noexpand\glo{}"
+                newtext = newtext.replace(
+                    word + "\glo{} ", wordReplacement + " ")
+                newtext = newtext.replace(
+                    word + "\glo{},", wordReplacement+",")
+                newtext = newtext.replace(
+                    word + "\glo{}.", wordReplacement+".")
+                newtext = newtext.replace(
+                    word + "\glo{}:", wordReplacement + ":")
+                newtext = newtext.replace(
+                    word + "\glo{}(", wordReplacement + "(")
+                newtext = newtext.replace(
+                    word + "\glo{})", wordReplacement + ")")
+                newtext = newtext.replace(
+                    word + "\glo{}}", wordReplacement + "}")
+                newtext = newtext.replace(
+                    word + "\glo{}\n", wordReplacement + "\n")
+                newtext = newtext.replace(
+                    word + "\glo{};", wordReplacement + ";")
+                newtext = newtext.replace(
+                    word + "\glo{}\"", wordReplacement + "\"")
+            textData = textData.replace(found, newtext)
+
     text.seek(0)
     text.truncate()
     text.write(textData)
     text.close()
-
